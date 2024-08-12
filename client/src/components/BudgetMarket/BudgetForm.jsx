420007bc7102d821b211bc5b398dade51ff9b9c3
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-12 21:18:37 +0200

    update btn css

--- client/src/components/BudgetMarket/BudgetForm.jsx
+++ client/src/components/BudgetMarket/BudgetForm.jsx
@@ -223,7 +223,22 @@ const BudgetForm = () => {
                             )}
                           </FieldArray>
                         )}
-                        <div className="flex justify-end">
+                        <div className="flex justify-between">
+                          <button
+                            type="button"
+                            onClick={() =>
+                              push({
+                                name: "",
+                                budget: 0,
+                                startYear: new Date().getFullYear(),
+                                endYear: new Date().getFullYear(),
+                                budgetPerYear: [],
+                              })
+                            }
+                            className="rounded-md bg-blue-500 px-2 py-1 text-white"
+                          >
+                            Add EH
+                          </button>
                           <button
                             type="button"
                             onClick={() => remove(index)}
@@ -234,7 +249,7 @@ const BudgetForm = () => {
                         </div>
                       </div>
                     ))}
-                  <button
+                  {/* <button
                     type="button"
                     onClick={() =>
                       push({
@@ -248,11 +263,50 @@ const BudgetForm = () => {
                     className="w-full rounded-md bg-blue-500 px-4 py-2 text-white"
                   >
                     Add EH
-                  </button>
+                  </button> */}
                 </div>
               )}
             </FieldArray>
-            <button
+            <div className="flex justify-between gap-14">
+              <button
+                type="reset"
+                className="w-full rounded-md bg-gray-500 px-4 py-2 text-white"
+              >
+                Reset
+              </button>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full rounded-md bg-green-500 px-4 py-2 text-white"
+              >
+                {isSubmitting ? (
+                  <span className="flex items-center justify-center">
+                    <svg
+                      className="mr-3 h-5 w-5 animate-spin"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      />
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      />
+                    </svg>
+                    Calculating...
+                  </span>
+                ) : (
+                  "Calculate"
+                )}
+              </button>
+            </div>
+            {/* <button
               type="submit"
               disabled={isSubmitting}
               className="w-full rounded-md bg-green-500 px-4 py-2 text-white"
@@ -288,7 +342,7 @@ const BudgetForm = () => {
               className="w-full rounded-md bg-gray-500 px-4 py-2 text-white"
             >
               Reset
-            </button>
+            </button> */}
           </Form>
         )}
       </Formik>
