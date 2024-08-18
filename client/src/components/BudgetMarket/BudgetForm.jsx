d22508dbf4d1654e5c39b56a6b7cd7882e978831
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-18 18:28:17 +0200

    refact from filed

--- client/src/components/BudgetMarket/BudgetForm.jsx
+++ client/src/components/BudgetMarket/BudgetForm.jsx
@@ -1,15 +1,11 @@
-import Decimal from "decimal.js"
-import { Formik, Form, Field, ErrorMessage } from "formik"
+import { Formik, Form } from "formik"
+import { LuListRestart } from "react-icons/lu"
+
 import { validationSchema } from "../../service/validation/MarketOverrunValidation"
 import { calculateBudget } from "../../service/budgetCalculationService"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
-import Tooltip from "../Tooltip/Tooltip"
-import { LuListRestart } from "react-icons/lu"
-import { MdCalculate } from "react-icons/md"
 import Title from "../PageTitle/PageTtile"
-import CustomDatePicker from "../CustomDatePicker/CustomDatePicker"
-import YearlyBudgetInputs from "../YearlyBudgetInputs/YearlyBudgetInputs"
+import FormFields from "./BudgetMarketComponents/FormFields"
+import SubmitButton from "./BudgetMarketComponents/SubmitButton"
 
 const INITIAL_VALUES = {
   remainder: "0",
@@ -26,194 +22,51 @@ const INITIAL_VALUES = {
 }
 
 const BudgetForm = () => {
+  const handleSubmit = async (values, { setSubmitting, setValues }) => {
+    setSubmitting(true)
+    try {
+      const result = await calculateBudget(values)
+      if (result) {
+        setValues({
+          ...values,
+          EH: {
+            ...values.EH,
+            calculatedBudget: result.calculatedBudget,
+            calculatedBudgetPerYear: result.calculatedBudgetPerYear,
+          },
+          remainder: result.remainder,
+          numberOfEHs: result.numberOfEHs,
+          currentEHNumber: result.currentEHNumber,
+        })
+      }
+    } catch (error) {
+      console.error("Error in form submission:", error)
+      alert("An error occurred while calculating the EH budget.")
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
   return (
     <>
-      <Title content={"Market Overrun Calculator"} />
+      <Title content="Market Overrun Calculator" />
       <Formik
         initialValues={INITIAL_VALUES}
         validationSchema={validationSchema}
-        // "penny pinching" algorithm
-        onSubmit={(values, { setSubmitting, setValues }) => {
-          setSubmitting(true)
-          try {
-            const result = calculateBudget(values)
-            if (result) {
-              setValues({
-                ...values,
-                EH: {
-                  ...values.EH,
-                  calculatedBudget: result.calculatedBudget,
-                  calculatedBudgetPerYear: result.calculatedBudgetPerYear,
-                },
-                remainder: result.remainder,
-                numberOfEHs: result.numberOfEHs,
-                currentEHNumber: result.currentEHNumber,
-              })
-            }
-          } catch (error) {
-            console.error("Error in form submission:", error)
-            alert("An error occurred while calculating the EH budget.")
-          } finally {
-            setSubmitting(false)
-          }
-        }}
+        onSubmit={handleSubmit}
       >
         {({ values, isSubmitting, setValues }) => (
           <Form className="mx-auto mt-8 max-w-lg space-y-6">
-            <article className="flex justify-between gap-3 rounded-md border border-zinc-400 bg-zinc-400/20 p-4">
-              <div className="flex-col gap-2">
-                <div className="flex gap-2">
-                  <div className="flex h-11 w-28 items-center rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-                    Remainder:
-                  </div>
-                  <Field
-                    name="remainder"
-                    type="number"
-                    placeholder="Remainder"
-                    className="w-44 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-                  />
-                </div>
-                <ErrorMessage
-                  name="remainder"
-                  component="div"
-                  className="flex justify-end text-sm text-red-500"
-                />
-              </div>
-              <div className="flex-col gap-2">
-                <div className="flex gap-2">
-                  <div className="flex h-11 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-                    Nb EHs:
-                  </div>
-                  <Field
-                    name="numberOfEHs"
-                    type="number"
-                    className="w-20 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-                  />
-                </div>
-                <ErrorMessage
-                  name="numberOfEHs"
-                  component="div"
-                  className="flex justify-end text-sm text-red-500"
-                />
-              </div>
-            </article>
-
-            <article className="space-y-4 rounded-md">
-              <div className="flex flex-col space-y-2 rounded-md border border-zinc-400 bg-zinc-400/20 p-4">
-                <div className="flex w-full justify-between gap-2">
-                  <div className="h-11 w-[175px] whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-                    EH Budget
-                    {values.currentEHNumber > 0 ? values.currentEHNumber : ""} :
-                  </div>
-                  <Field
-                    name="EH.budget"
-                    type="number"
-                    placeholder="EH Budget"
-                    className="w-36 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-                  />
-                  <Tooltip
-                    content={new Decimal(
-                      values.EH.calculatedBudget || values.EH.budget || 0,
-                    ).toString()}
-                  >
-                    <div className="flex h-11 w-36 items-center justify-center rounded-md border bg-white px-3 font-semibold text-teal-600 dark:bg-zinc-500 dark:text-teal-200">
-                      {new Decimal(
-                        values.EH.calculatedBudget || values.EH.budget || 0,
-                      ).toFixed(2)}
-                    </div>
-                  </Tooltip>
-                </div>
-                <ErrorMessage
-                  name="EH.budget"
-                  component="div"
-                  className="text-sm text-red-500"
-                />
-                <div className="flex gap-2">
-                  <div className="h-11 w-full whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-                    Market duration:
-                  </div>
-                  <div className="flex gap-2">
-                    <CustomDatePicker
-                      selected={values.EH.startDate}
-                      onChange={(date) => {
-                        setValues((prevValues) => ({
-                          ...prevValues,
-                          EH: {
-                            ...prevValues.EH,
-                            startDate: date,
-                          },
-                        }))
-                      }}
-                      name="EH.startDate"
-                    />
-                    <CustomDatePicker
-                      selected={values.EH.endDate}
-                      onChange={(date) => {
-                        setValues((prevValues) => ({
-                          ...prevValues,
-                          EH: {
-                            ...prevValues.EH,
-                            endDate: date,
-                          },
-                        }))
-                      }}
-                      name="EH.endDate"
-                    />
-                  </div>
-                </div>
-                <div className="py-1">
-                  <hr className="border-t border-zinc-400 dark:border-zinc-500" />
-                </div>
-                <YearlyBudgetInputs
-                  startDate={values.EH.startDate}
-                  endDate={values.EH.endDate}
-                  budgetPerYear={values.EH.budgetPerYear}
-                  calculatedBudgetPerYear={values.EH.calculatedBudgetPerYear}
-                />
-              </div>
-            </article>
-            <article className="flex justify-between gap-14">
+            <FormFields values={values} setValues={setValues} />
+            <div className="flex justify-between gap-14">
               <button
                 type="reset"
                 className="flex h-11 w-full items-center justify-center gap-4 rounded-md border border-zinc-900 bg-gradient-to-r from-zinc-700 via-zinc-500 to-zinc-700 px-4 py-2 text-lg font-semibold text-zinc-200 hover:from-zinc-400 hover:via-zinc-700 hover:to-zinc-400"
               >
                 <LuListRestart className="size-6" /> Reset
               </button>
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="flex h-11 w-full items-center justify-center gap-4 rounded-md border border-teal-900 bg-gradient-to-r from-teal-700 via-teal-500 to-teal-700 px-4 py-2 text-lg font-semibold text-zinc-200 hover:from-teal-400 hover:via-teal-700 hover:to-teal-400"
-              >
-                {isSubmitting ? (
-                  <span className="flex items-center justify-center">
-                    <svg
-                      className="mr-3 h-5 w-5 animate-spin"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      />
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      />
-                    </svg>
-                    Calculating...
-                  </span>
-                ) : (
-                  <>
-                    <MdCalculate className="size-6" />
-                    Calculate
-                  </>
-                )}
-              </button>
-            </article>
+              <SubmitButton isSubmitting={isSubmitting} />
+            </div>
           </Form>
         )}
       </Formik>
--- client/src/components/BudgetMarket/BudgetMarketComponents/FormFields.jsx
+++ client/src/components/BudgetMarket/BudgetMarketComponents/FormFields.jsx
@@ -5,59 +5,56 @@ import CustomDatePicker from "../../CustomDatePicker/CustomDatePicker"
 import YearlyBudgetInputs from "../../YearlyBudgetInputs/YearlyBudgetInputs"
 
 const FormFields = ({ values, setValues }) => {
+  const handleDateChange = (field) => (date) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      EH: {
+        ...prevValues.EH,
+        [field]: date,
+      },
+    }))
+  }
+
+  const renderField = (name, label, placeholder, labelWidth, width) => (
+    <div className="flex-col gap-2">
+      <div className="flex gap-2">
+        <div
+          className={`${labelWidth} flex h-11 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10`}
+        >
+          {label}
+        </div>
+        <Field
+          name={name}
+          type="number"
+          placeholder={placeholder}
+          className={`${width} rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200`}
+        />
+      </div>
+      <ErrorMessage
+        name={name}
+        component="div"
+        className="flex justify-end text-sm text-red-500"
+      />
+    </div>
+  )
+
   return (
     <>
       <article className="flex justify-between gap-3 rounded-md border border-zinc-400 bg-zinc-400/20 p-4">
-        <div className="flex-col gap-2">
-          <div className="flex gap-2">
-            <div className="flex h-11 w-28 items-center rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-              Remainder:
-            </div>
-            <Field
-              name="remainder"
-              type="number"
-              placeholder="Remainder"
-              className="w-44 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-            />
-          </div>
-          <ErrorMessage
-            name="remainder"
-            component="div"
-            className="flex justify-end text-sm text-red-500"
-          />
-        </div>
-        <div className="flex-col gap-2">
-          <div className="flex gap-2">
-            <div className="flex h-11 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-              Nb EHs:
-            </div>
-            <Field
-              name="numberOfEHs"
-              type="number"
-              className="w-20 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-            />
-          </div>
-          <ErrorMessage
-            name="numberOfEHs"
-            component="div"
-            className="flex justify-end text-sm text-red-500"
-          />
-        </div>
+        {renderField("remainder", "Remainder:", "Remainder", null, "w-44")}
+        {renderField("numberOfEHs", "Nb EHs:", null, null, "w-20")}
       </article>
 
       <article className="space-y-4 rounded-md">
         <div className="flex flex-col space-y-2 rounded-md border border-zinc-400 bg-zinc-400/20 p-4">
           <div className="flex w-full justify-between gap-2">
-            <div className="h-11 w-[175px] whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
-              EH Budget
-              {values.currentEHNumber > 0 ? values.currentEHNumber : ""} :
-            </div>
-            <Field
-              name="EH.budget"
-              type="number"
-              placeholder="EH Budget"
-              className="w-36 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
-            />
+            {renderField(
+              "EH.budget",
+              `EH Budget${values.currentEHNumber > 0 ? ` ${values.currentEHNumber}` : ""}:`,
+              "Enter budget",
+              "w-44",
+              "w-36",
+            )}
             <Tooltip
               content={new Decimal(
                 values.EH.calculatedBudget || values.EH.budget || 0,
@@ -70,11 +67,6 @@ const FormFields = ({ values, setValues }) => {
               </div>
             </Tooltip>
           </div>
-          <ErrorMessage
-            name="EH.budget"
-            component="div"
-            className="text-sm text-red-500"
-          />
           <div className="flex gap-2">
             <div className="h-11 w-full whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
               Market duration:
@@ -82,35 +74,17 @@ const FormFields = ({ values, setValues }) => {
             <div className="flex gap-2">
               <CustomDatePicker
                 selected={values.EH.startDate}
-                onChange={(date) => {
-                  setValues((prevValues) => ({
-                    ...prevValues,
-                    EH: {
-                      ...prevValues.EH,
-                      startDate: date,
-                    },
-                  }))
-                }}
+                onChange={handleDateChange("startDate")}
                 name="EH.startDate"
               />
               <CustomDatePicker
                 selected={values.EH.endDate}
-                onChange={(date) => {
-                  setValues((prevValues) => ({
-                    ...prevValues,
-                    EH: {
-                      ...prevValues.EH,
-                      endDate: date,
-                    },
-                  }))
-                }}
+                onChange={handleDateChange("endDate")}
                 name="EH.endDate"
               />
             </div>
           </div>
-          <div className="py-1">
-            <hr className="border-t border-zinc-400 dark:border-zinc-500" />
-          </div>
+          <hr className="border-t border-zinc-400 dark:border-zinc-500" />
           <YearlyBudgetInputs
             startDate={values.EH.startDate}
             endDate={values.EH.endDate}
