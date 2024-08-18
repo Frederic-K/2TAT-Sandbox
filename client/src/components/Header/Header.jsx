9b1d99621627f017e1c3b0369f39b6467acb784a
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-18 12:12:23 +0200

    update header footer border css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import ThemeToggle from "../ThemeToggle/ThemeToggle"
 
 const Header = () => {
   return (
-    <div className="mx-auto mb-4 flex h-12 items-center justify-end bg-[url('/images/background/waveBanner-1.webp')] bg-cover bg-center bg-no-repeat text-zinc-200 dark:bg-zinc-800">
+    <div className="mx-auto mb-4 flex h-12 items-center justify-end border-b-2 border-zinc-600/30 bg-[url('/images/background/waveBanner-1.webp')] bg-cover bg-center bg-no-repeat text-zinc-200 dark:bg-zinc-800">
       <ThemeToggle />
     </div>
   )
