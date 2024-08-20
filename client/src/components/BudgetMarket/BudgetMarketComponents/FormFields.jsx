3ef797d6e876b6487caad6ea1349fb8e782b3249
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-20 17:30:48 +0200

    give up renderField

--- client/src/components/BudgetMarket/BudgetMarketComponents/FormFields.jsx
+++ client/src/components/BudgetMarket/BudgetMarketComponents/FormFields.jsx
@@ -16,58 +16,68 @@ const FormFields = ({ values, setValues }) => {
     }))
   }
 
-  const renderField = (name, label, placeholder, labelWidth, width) => (
-    <div className="grid gap-2">
-      <div className="grid grid-cols-[auto,1fr] gap-2">
-        <div
-          className={`${labelWidth} flex h-11 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10`}
-        >
-          {label}
-        </div>
-        <Field
-          name={name}
-          type="number"
-          placeholder={placeholder}
-          className={`${width} rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200`}
-        />
-      </div>
-      <ErrorMessage
-        name={name}
-        component="div"
-        className="text-right text-sm text-red-500"
-      />
-    </div>
-  )
-
   return (
     <>
       <article className="grid grid-cols-1 gap-3 rounded-md border border-zinc-400 bg-zinc-400/20 p-4 sm:grid-cols-[2fr,1fr]">
-        {renderField(
-          "remainder",
-          "Remainder:",
-          "Remainder",
-          "w-32 sm:w-full",
-          "w-full",
-        )}
-        {renderField(
-          "numberOfEHs",
-          "Nb EHs:",
-          null,
-          "w-32 sm:w-full",
-          "w-full",
-        )}
+        <div className="grid gap-2">
+          <div className="grid grid-cols-[auto,1fr] gap-2">
+            <div className="flex h-11 w-32 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 sm:w-full dark:border-zinc-300 dark:bg-zinc-200/10">
+              Remainder:
+            </div>
+            <Field
+              name="remainder"
+              type="number"
+              placeholder="Remainder"
+              className="w-full rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
+            />
+          </div>
+          <ErrorMessage
+            name="remainder"
+            component="div"
+            className="text-right text-sm text-red-500"
+          />
+        </div>
+
+        <div className="grid gap-2">
+          <div className="grid grid-cols-[auto,1fr] gap-2">
+            <div className="flex h-11 w-32 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 sm:w-full dark:border-zinc-300 dark:bg-zinc-200/10">
+              Nb EHs:
+            </div>
+            <Field
+              name="numberOfEHs"
+              type="number"
+              className="w-full rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
+            />
+          </div>
+          <ErrorMessage
+            name="numberOfEHs"
+            component="div"
+            className="text-right text-sm text-red-500"
+          />
+        </div>
       </article>
 
       <article className="grid gap-4 rounded-md">
         <div className="grid gap-2 rounded-md border border-zinc-400 bg-zinc-400/20 p-4">
           <div className="grid grid-cols-[1fr,auto] gap-2">
-            {renderField(
-              "EH.budget",
-              `EH Budget${values.currentEHNumber > 0 ? ` ${values.currentEHNumber}` : ""}:`,
-              "Enter budget",
-              "w-44",
-              "w-36",
-            )}
+            <div className="grid gap-2">
+              <div className="grid grid-cols-[auto,1fr] gap-2">
+                <div className="flex h-11 w-44 items-center whitespace-nowrap rounded-md border border-zinc-600 bg-zinc-400/20 px-3 py-2 font-semibold text-orange-600 dark:border-zinc-300 dark:bg-zinc-200/10">
+                  {`EH Budget${values.currentEHNumber > 0 ? ` ${values.currentEHNumber}` : ""}:`}
+                </div>
+                <Field
+                  name="EH.budget"
+                  type="number"
+                  placeholder="Enter budget"
+                  className="w-36 rounded-md border px-3 py-2 dark:bg-zinc-500 dark:text-zinc-200"
+                />
+              </div>
+              <ErrorMessage
+                name="EH.budget"
+                component="div"
+                className="text-right text-sm text-red-500"
+              />
+            </div>
             <Tooltip
               content={formatNumber(
                 new Decimal(
