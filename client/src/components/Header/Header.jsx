23ca85aa0ad16ccc905542fed641a983b6be2fa3
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-21 19:21:47 +0200

    colapse menu  issue fixed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         className="flex cursor-pointer flex-col justify-center px-4"
       >
         <BurgerMenu props={{ setIsOpen, isOpen }} />
-        <div className="absolute left-0 top-12 w-full origin-top-right scale-y-0 bg-zinc-800 py-2 transition-transform peer-open:z-30 peer-open:flex peer-open:origin-top-right peer-open:scale-y-100 peer-open:flex-col peer-open:items-center peer-open:justify-center">
+        <div className="absolute left-0 top-12 flex w-full origin-top-right scale-y-0 flex-col items-center justify-center bg-zinc-800 py-2 transition-transform peer-open:z-30 peer-open:scale-y-100">
           <NavLinks setIsOpen={setIsOpen} />
         </div>
       </div>
