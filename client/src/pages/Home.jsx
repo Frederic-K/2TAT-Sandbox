45110f838265a58696ed5743480f4a48d1beafa3
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-08-31 09:47:16 +0200

    set home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,22 @@
-import BudgetForm from "../components/BudgetMarket/BudgetForm"
-import EHList from "../components/EHList/EHList"
-import Notes from "../components/Notes/Notes"
+import { NavLink } from "react-router-dom"
+import Title from "../components/PageTitle/PageTtile"
 
 const Home = () => {
+  const NAVLINKS = [
+    { label: "Text Formatter", path: "/textformatter" },
+    { label: "EH Calculator", path: "/ehcalculator" },
+    // Add more navigation links as needed
+  ]
+
   return (
-    <main className="min-h-screen">
-      <BudgetForm />
-      <EHList />
-      <Notes />
+    <main className="mt-12 min-h-screen">
+      <div className="flex flex-col gap-4">
+        {NAVLINKS.map(({ label, path }) => (
+          <NavLink key={label} to={path}>
+            <Title content={label} />
+          </NavLink>
+        ))}
+      </div>
     </main>
   )
 }
