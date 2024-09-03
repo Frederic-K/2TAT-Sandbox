3e3a2034f9cdf18d3c16cba904b05af8618d0382
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-09-03 20:32:21 +0200

    add toaster for formatted text copied

--- client/src/pages/TextFormatter.jsx
+++ client/src/pages/TextFormatter.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import { LuListRestart } from "react-icons/lu"
 import { ToggleSwitch } from "flowbite-react"
+import { Toaster } from "react-hot-toast"
+import toast from "react-hot-toast"
 
 import Title from "../components/PageTitle/PageTtile"
 
@@ -62,64 +64,67 @@ const TextFormatter = () => {
     const data = [new ClipboardItem({ "text/html": blob })]
     navigator.clipboard
       .write(data)
-      .then(() => alert("Formatted text copied to clipboard!"))
+      .then(() => toast.success("Formatted text copied to clipboard!"))
       .catch((err) => console.error("Failed to copy text: ", err))
   }
 
   return (
-    <div className="mx-auto min-h-screen max-w-lg px-4">
-      <Title content="Text Formatter" />
-      <div className="mb-1 mt-4">
-        <label className="mb-2 block text-xl font-semibold text-orange-700">
-          Paste your text here:
-        </label>
-        <textarea
-          className="h-40 w-full rounded border-2 border-zinc-300 p-2 font-mono dark:bg-zinc-300 dark:text-zinc-700"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="mb-4 flex h-11 items-center justify-end">
-        {!autoFormat && (
+    <>
+      <Toaster position="bottom-left" reverseOrder={false} />
+      <div className="mx-auto min-h-screen max-w-lg px-4">
+        <Title content="Text Formatter" />
+        <div className="mb-1 mt-4">
+          <label className="mb-2 block text-xl font-semibold text-orange-700">
+            Paste your text here:
+          </label>
+          <textarea
+            className="h-40 w-full rounded border-2 border-zinc-300 p-2 font-mono dark:bg-zinc-300 dark:text-zinc-700"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4 flex h-11 items-center justify-end">
+          {!autoFormat && (
+            <button
+              className="mr-auto flex h-11 items-center justify-center rounded-md border border-orange-900 bg-gradient-to-r from-orange-700 via-orange-500 to-orange-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-orange-400 hover:via-orange-700 hover:to-orange-400"
+              onClick={formatText}
+            >
+              Format Text
+            </button>
+          )}
+          <div className="flex items-center">
+            <span className="mr-2 text-zinc-500 dark:text-zinc-300">
+              Auto Format: {autoFormat ? "ON" : "OFF"}
+            </span>
+            <ToggleSwitch checked={autoFormat} onChange={setAutoFormat} />
+          </div>
+        </div>
+        <div className="mb-1 mt-4">
+          <label className="mb-2 block text-xl font-semibold text-orange-700">
+            Formatted text:
+          </label>
+          <textarea
+            className="h-40 w-full rounded border-2 border-zinc-300 p-2 font-mono dark:bg-zinc-300 dark:text-zinc-700"
+            value={formattedText}
+            readOnly
+          />
+        </div>
+        <div className="flex items-center justify-between">
           <button
-            className="mr-auto flex h-11 items-center justify-center rounded-md border border-orange-900 bg-gradient-to-r from-orange-700 via-orange-500 to-orange-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-orange-400 hover:via-orange-700 hover:to-orange-400"
-            onClick={formatText}
+            className="flex h-11 items-center justify-center rounded-md border border-teal-900 bg-gradient-to-r from-teal-700 via-teal-500 to-teal-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-teal-400 hover:via-teal-700 hover:to-teal-400"
+            onClick={copyToClipboard}
           >
-            Format Text
+            Copy Formatted Text
           </button>
-        )}
-        <div className="flex items-center">
-          <span className="mr-2 text-zinc-500 dark:text-zinc-300">
-            Auto Format: {autoFormat ? "ON" : "OFF"}
-          </span>
-          <ToggleSwitch checked={autoFormat} onChange={setAutoFormat} />
+          <button
+            className="flex h-11 items-center justify-center gap-2 rounded-md border border-zinc-900 bg-gradient-to-r from-zinc-700 via-zinc-500 to-zinc-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-zinc-400 hover:via-zinc-700 hover:to-zinc-400"
+            onClick={resetForm}
+          >
+            <LuListRestart className="size-6" /> Reset
+          </button>
         </div>
       </div>
-      <div className="mb-1 mt-4">
-        <label className="mb-2 block text-xl font-semibold text-orange-700">
-          Formatted text:
-        </label>
-        <textarea
-          className="h-40 w-full rounded border-2 border-zinc-300 p-2 font-mono dark:bg-zinc-300 dark:text-zinc-700"
-          value={formattedText}
-          readOnly
-        />
-      </div>
-      <div className="flex items-center justify-between">
-        <button
-          className="flex h-11 items-center justify-center rounded-md border border-teal-900 bg-gradient-to-r from-teal-700 via-teal-500 to-teal-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-teal-400 hover:via-teal-700 hover:to-teal-400"
-          onClick={copyToClipboard}
-        >
-          Copy Formatted Text
-        </button>
-        <button
-          className="flex h-11 items-center justify-center gap-2 rounded-md border border-zinc-900 bg-gradient-to-r from-zinc-700 via-zinc-500 to-zinc-700 px-4 py-2 text-lg font-semibold text-zinc-100 hover:from-zinc-400 hover:via-zinc-700 hover:to-zinc-400"
-          onClick={resetForm}
-        >
-          <LuListRestart className="size-6" /> Reset
-        </button>
-      </div>
-    </div>
+    </>
   )
 }
 
