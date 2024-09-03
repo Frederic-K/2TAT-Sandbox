e2e57919bb9c9c6bc0f7ba90a2d5eebcd8c92a1f
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-09-03 19:17:44 +0200

    set home Button to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect, useCallback } from "react"
+import { Link } from "react-router-dom"
 import ThemeToggle from "../ThemeToggle/ThemeToggle"
 import BurgerMenu from "./HeaderComponents/BurgerMenu"
 import NavLinks from "./HeaderComponents/NavLinks"
@@ -23,17 +24,31 @@ const Header = () => {
   }, [isOpen, handleClickOutside])
 
   return (
-    <div className="relative mx-auto mb-6 flex h-12 items-center justify-end border-b-2 border-zinc-600/30 bg-[url('/images/background/waveBanner-1.webp')] bg-cover bg-center bg-no-repeat dark:bg-zinc-800">
-      <ThemeToggle />
-      <div
-        ref={menuRef}
-        className="flex cursor-pointer flex-col justify-center px-4"
-      >
-        <BurgerMenu props={{ setIsOpen, isOpen }} />
-        <div className="absolute left-0 top-12 flex w-full origin-top-right scale-y-0 flex-col items-center justify-center bg-zinc-800 py-2 transition-transform peer-open:z-30 peer-open:scale-y-100">
-          <NavLinks setIsOpen={setIsOpen} />
+    <div className="relative mx-auto mb-6 flex h-12 items-center justify-between border-b-2 border-zinc-600/30 bg-[url('/images/background/waveBanner-1.webp')] bg-cover bg-center bg-no-repeat dark:bg-zinc-800">
+      <section className="group relative my-auto ml-4">
+        <Link to="/" className="flex items-center">
+          <img
+            src="/images/logo/LogoFK-waterpx-red3-100px.png"
+            alt="Logo"
+            className="mt-1 size-9 cursor-pointer transition-transform hover:rotate-[360deg]"
+          />
+          <span className="absolute left-full ml-2 whitespace-nowrap font-semibold text-orange-500 opacity-0 transition-opacity group-hover:opacity-100">
+            Home
+          </span>
+        </Link>
+      </section>
+      <section className="flex">
+        <ThemeToggle />
+        <div
+          ref={menuRef}
+          className="flex cursor-pointer flex-col justify-center px-4"
+        >
+          <BurgerMenu props={{ setIsOpen, isOpen }} />
+          <div className="absolute left-0 top-12 flex w-full origin-top-right scale-y-0 flex-col items-center justify-center bg-zinc-800 py-2 transition-transform peer-open:z-30 peer-open:scale-y-100">
+            <NavLinks setIsOpen={setIsOpen} />
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
