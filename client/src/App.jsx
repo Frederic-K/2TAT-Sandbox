d2d6ce4c4181f0fad607777c66780bdb9ca78ae0
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-10-17 18:58:03 +0200

    init animated presence

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,39 @@
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// import RootLayout from "./components/RootLayout/RootLayout"
+
+// import Home from "./pages/Home"
+// import TextFormatter from "./pages/TextFormatter"
+// import MarketCalculator from "./pages/MarketCalculator"
+// import NotFound from "./pages/NotFound"
+
+// const publicRoutes = [
+//   { index: true, element: <Home /> },
+//   { path: "marketcalculator", element: <MarketCalculator /> },
+//   { path: "textformatter", element: <TextFormatter /> },
+// ]
+
+// function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <RootLayout />,
+//       errorElement: <NotFound />,
+//       children: [...publicRoutes],
+//     },
+//   ])
+
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   )
+// }
+
+// export default App
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
 
 import RootLayout from "./components/RootLayout/RootLayout"
 
@@ -24,9 +59,9 @@ function App() {
   ])
 
   return (
-    <>
+    <AnimatePresence mode="wait">
       <RouterProvider router={router} />
-    </>
+    </AnimatePresence>
   )
 }
 
--- client/src/components/RootLayout/RootLayout.jsx
+++ client/src/components/RootLayout/RootLayout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
+import { AnimatePresence } from "framer-motion"
 
 const RootLayout = () => {
+  const location = useLocation()
+
   return (
     <>
       <Header />
-      <Outlet />
+      <AnimatePresence mode="wait">
+        <Outlet key={location.pathname} />
+      </AnimatePresence>
       <Footer />
     </>
   )
